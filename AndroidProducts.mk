2c298fc24f714fba6ac4f7b47d573e4c7474e583
David <davidmolinamorales1999@gmail.com>
2021-04-07 00:03:55 +0200

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_kyleproxx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_kyleproxx-user \
+    lineage_kyleproxx-userdebug \
+    lineage_kyleproxx-eng
