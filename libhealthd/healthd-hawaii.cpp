c32e5a3f6d1dcd6ded557167067e01ff5064d756
David <davidmolinamorales1999@gmail.com>
2021-04-07 00:52:21 +0200

    Update healthd-hawaii.cpp

--- libhealthd/healthd-hawaii.cpp
+++ libhealthd/healthd-hawaii.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include "healthd/healthd.h"
 
 void
 healthd_board_init(struct healthd_config *config)
